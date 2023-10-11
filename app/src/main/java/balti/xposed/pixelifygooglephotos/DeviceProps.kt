f642719bf1107359fbfdcc00cb14c72f56a0f21c
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2023-10-12 00:20:59 +0530

    Update DeviceProps.kt

--- app/src/main/java/balti/xposed/pixelifygooglephotos/DeviceProps.kt
+++ app/src/main/java/balti/xposed/pixelifygooglephotos/DeviceProps.kt
@@ -49,10 +49,15 @@ object DeviceProps {
      */
     val allFeatures = listOf(
 
+        Features("Pixel",
+            "com.google.android.feature.PIXEL_EXPERIENCE",
+            "com.google.android.feature.GOOGLE_BUILD",
+            "com.google.android.feature.GOOGLE_EXPERIENCE",
+        ),
+
         Features("Pixel 2016",
             "com.google.android.apps.photos.NEXUS_PRELOAD",
             "com.google.android.apps.photos.nexus_preload",
-            "com.google.android.feature.PIXEL_EXPERIENCE",
             "com.google.android.apps.photos.PIXEL_PRELOAD",
             "com.google.android.apps.photos.PIXEL_2016_PRELOAD",
         ),
@@ -173,7 +178,7 @@ object DeviceProps {
         AndroidVersion("R 11.0", "11", 30),
         AndroidVersion("S 12.0", "12", 31),
         AndroidVersion("T 13.0", "13", 33),
-        AndroidVersion("U 14.0", "14", 34),
+        AndroidVersion("UD 14.0", "14", 34),
     )
 
     /**
@@ -351,10 +356,16 @@ object DeviceProps {
                 Pair("PRODUCT", "husky"),
                 Pair("MODEL", "Pixel 8 Pro"),
                 Pair("ID", "UD1A.230803.041"),
+                Pair("DESCRIPTION", "husky-user 14 UD1A.230803.041 10808477 release-keys"),
                 Pair("FINGERPRINT", "google/husky/husky:14/UD1A.230803.041/10808477:user/release-keys"),
+                Pair("HARDWARE", "husky"),
+                Pair("BOARD", "husky"),
+                Pair("PLATFORM", "zuma"),
+                Pair("BOOTLOADER", "ripcurrent-14.0-10807316"),
+                Pair("INCREMENTAL", "10808477"),
             ),
-            "Pixel 2023",
-            getAndroidVersionFromLabel("U 14.0"),
+            "Pixel 2023 mid-year",
+            getAndroidVersionFromLabel("UD 14.0"),
         ),
     )
 
@@ -376,11 +387,11 @@ object DeviceProps {
     /**
      * Default name of device to spoof.
      */
-    val defaultDeviceName = "Pixel XL"
+    val defaultDeviceName = "Pixel 8 Pro"
 
     /**
      * Default feature level to spoof up to. Corresponds to what is expected for the device in [defaultDeviceName].
      */
-    val defaultFeatures = getFeaturesUpTo("Pixel 2016")
+    val defaultFeatures = getFeaturesUpTo("Pixel 2023 mid-year")
 
 }
